d5846ed418be6026447cfa1051a8b66e99e4aa27
Brian Rian Rehan <80043480+brianrianrehan@users.noreply.github.com>
2022-08-06 16:38:59 +0700

    Create Xmr.sh

---
+++ Xmr.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Install XRDP
+
+# Before get xmr coin for free
+
+# Google Colab
+
+sudo apt update
+
+clear
+
+sudo apt install screen
+
+screen -R xmr
+
+ 
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+
+cd xmrig-6.17.0
+
+./xmrig -o us-west.minexmr.com:443 -u 83heT2c1ppw9fdHPW5drK5MCgCn9dGS8ZY91AC6vvpryJiLsZrDUaBkf3vJzRWviEXUaaoGUsuqP8PrLCTFE5kWJ7TsCeDQ -k --tls --rig-id GC-US
